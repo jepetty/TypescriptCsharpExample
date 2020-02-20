eba2246028aa2e67ef3b04d808e28b0c62db8d35
Jessica Petty <jepetty@microsoft.com>
2020-02-20 10:44:35 -0800

    Add silly json parsing for example

---
+++ TypescriptCsharpExample/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TypescriptCsharpExample
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var myString = "{\"name\": \"jessica\"}";
+            var myJsonObject = JToken.Parse(myString);
+            var finalString = JsonConvert.SerializeObject(myJsonObject);
+            Console.WriteLine(finalString);
+            return View();
+        }
+    }
+}
