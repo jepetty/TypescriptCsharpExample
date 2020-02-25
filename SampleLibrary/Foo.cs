6fbf3698bda2b370c86ec8eec349968c0755b9e9
Jessica Petty <jepetty@microsoft.com>
2020-02-25 09:08:54 -0800

    Adding reference to another project in same repo

---
+++ SampleLibrary/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    public static class Foo
+    {
+        public static string WriteNiceThing()
+        {
+            var niceThing = "You are so awesome!";
+            Console.WriteLine(niceThing);
+
+            return niceThing;
+        }
+    }
+}
--- TypescriptCsharpExample/HomeController.cs
+++ TypescriptCsharpExample/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SampleLibrary;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,6 +20,9 @@ namespace TypescriptCsharpExample
             var myJsonObject = JToken.Parse(myString);
             var finalString = JsonConvert.SerializeObject(myJsonObject);
             Console.WriteLine(finalString);
+
+            Foo.WriteNiceThing();
+
             return View();
         }
     }
